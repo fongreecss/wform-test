87e47da94d5e10114856e1b316ed35ecaae4acec
Aleksander Fras <fongreecss@gmail.com>
2023-02-10 11:40:49 +0100

    commit z buildom

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,18 @@ namespace wform_test
 {
     public partial class Form1 : Form
     {
+        /**
+         * KONSTRUKTOR RAZREDA 
+         * 
+         */
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
